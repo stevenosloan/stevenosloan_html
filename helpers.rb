d83b316354a51f6d22bd9f2a1b48ae55890bc046
Steven Sloan <stevenosloan@gmail.com>
2012-10-01 23:46:50 -0400

    use chomp for versioned script helper

--- helpers.rb
+++ helpers.rb
@@ -2,7 +2,7 @@ module CustomHelpers
 
   def versioned_script( script_name )
     tag = javascript_include_tag "#{script_name}"
-    tag.gsub('<script src="', '' ).gsub('" type="text/javascript"></script>', '' )
+    tag.chomp('<script src="').chomp('" type="text/javascript"></script>')
   end
 
   def render_partial( partial_name )
