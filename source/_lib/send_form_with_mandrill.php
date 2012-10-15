ee0eb0f1edfae8511a063fa7467745f5517d8724
Steven Sloan <stevenosloan@gmail.com>
2012-10-15 02:20:17 -0400

    WIP contact page

---
+++ source/_lib/send_form_with_mandrill.php
@@ -0,0 +1,13 @@
+<?php
+	if( isset( $_POST["submit"] ) ){
+
+		/*	Setup Form Variables
+		------------------------------------------*/
+		$name = $_POST["name"];
+		$email = $_POST["email"];
+		$message = $_POST["message"];
+		$honeypot = $_POST["honeypot"];
+
+		
+	}
+?>
