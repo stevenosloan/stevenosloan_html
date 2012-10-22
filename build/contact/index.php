c74336efdc720261f0f298395d6437c946c5ca15
Steven Sloan <stevenosloan@gmail.com>
2012-10-22 00:47:17 -0400

    build neue

---
+++ build/contact/index.php
@@ -0,0 +1,41 @@
+<?php
+	$base = $_SERVER['DOCUMENT_ROOT'] . '/source/';
+	require_once $base . "_lib/send_form_with_mandrill.php";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='UTF-8' />
+    <meta content='width=device-width, initial-scale=1' name='viewport' />
+    <meta content='IE=Edge,chrome=1' http-equiv='X-UA-Compatible' />
+    <meta content='7KgHTNkMMzdgvgz_Jbb_uNkC18FuunUTvC7MxXWRVRo' name='google-site-verification' />
+    <link href='/feed.xml' rel='alternate' title='Steven Sloan | Designer &amp; Developer' type='application/rss+xml' />
+    <title>Contact | Steven Sloan | Steven Sloan</title>
+    <link href="/assets/css/site-58a8412c.css" media="screen" rel="stylesheet" type="text/css" />
+    <script src="/assets/scripts/init-67cf6598.js" type="text/javascript"></script>
+    <script type='text/javascript'>
+      Modernizr.load([ ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js']);
+      
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-9014377-1']);
+      _gaq.push(['_trackPageview']);
+      _gaq.push(['_trackPageLoadTime']);
+    </script>
+    
+  </head>
+  <body class='page-contact'>
+    <header class='site-navi'>
+      <a class='site-title' href='/'>
+        <h1>Steven Sloan</h1>
+      </a>
+      <nav>
+        <a class='' href='#anchor-articles'>articles</a>
+        <a class='is-here' href='#anchor-contact'>contact</a>
+      </nav>
+    </header>
+    <section class='content'>
+      <h1>Get in touch</h1>
+    </section>
+    <div id='grid'></div>
+  </body>
+</html>
