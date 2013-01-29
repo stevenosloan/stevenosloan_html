28bd3d63c007a6c3e95037643538fc9d748d3453
Steven Sloan <stevenosloan@gmail.com>
2013-01-29 00:12:52 -0500

    whitespace

--- config.rb
+++ config.rb
@@ -55,28 +55,28 @@ unless self.build?
 	use Rack::Rewrite do
 	  rewrite %r{(.*/$)}, lambda {|match, rack_env|
 
-	  	## if the file exists in source, serve it
-	  	if File.exists?( File.join( source_dir, rack_env['PATH_INFO'], 'index.php' ) )
-	      return File.join( 'source', rack_env['PATH_INFO'] + 'index.php' )
+			## if the file exists in source, serve it
+			if File.exists?( File.join( source_dir, rack_env['PATH_INFO'], 'index.php' ) )
+				return File.join( 'source', rack_env['PATH_INFO'] + 'index.php' )
 
-	    ## else if it only exists in build, build it
-	    elsif File.exists?( File.join( build_dir, rack_env['PATH_INFO'], 'index.php' ) )
+			## else if it only exists in build, build it
+			elsif File.exists?( File.join( build_dir, rack_env['PATH_INFO'], 'index.php' ) )
 
-	    	## remove the leading '/' from name
-	    	file = File.join( rack_env['PATH_INFO'], 'index.php' )
-	    	file[0] = ""
-	    	
-	    	## build the requested file
-	    	puts "building: #{File.join( 'build', rack_env['PATH_INFO'], 'index.php' )}"
-	    	%x[ middleman build -g #{file} ]
-	    	
-	    	## pass the path on
-	    	return File.join( 'build', rack_env['PATH_INFO'], 'index.php' )
-	   
-	   	## else it's an html file, so pass that on
-	    else
-	    	return rack_env['PATH_INFO'] + 'index.html'
-	    end
+				## remove the leading '/' from name
+				file = File.join( rack_env['PATH_INFO'], 'index.php' )
+				file[0] = ""
+
+				## build the requested file
+				puts "building: #{File.join( 'build', rack_env['PATH_INFO'], 'index.php' )}"
+				%x[ middleman build -g #{file} ]
+
+				## pass the path on
+				return File.join( 'build', rack_env['PATH_INFO'], 'index.php' )
+
+			## else it's an html file, so pass that on
+			else
+				return rack_env['PATH_INFO'] + 'index.html'
+			end
 	  }
 
 	end
@@ -101,17 +101,17 @@ configure :build do
 
   # Change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # Enable cache buster
   # Don't give a hash to assets in the static folder
   activate :asset_hash, :ignore => %r{^(assets/static/.*)}
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Use a different image path
   # set :http_path, "/Content/images/"
 
